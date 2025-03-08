3933f44227adac2569fe0c9c24ea7d02d3835685
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2025-03-08 06:47:47 -0500

    removed project from vitepress branch

--- src/components/ui/image-placeholder.tsx
+++
@@ -1,7 +0,0 @@
-import { Center, type CenterProps, Image } from '@chakra-ui/react'
-
-export const ImagePlaceholder = (props: CenterProps) => (
-  <Center w="full" h="full" bg="bg.muted" color="fg.subtle" {...props}>
-    <Image alt='jasenc.dev logo' src='./assets/207250834_padded_logo.png' rounded="lg"/>
-  </Center>
-)
